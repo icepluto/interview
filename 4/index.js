11908d246611c496dac7387b1d9fce5d32588c0d
icepluto <18792689010@163.com>
2019-04-29 11:54:41 +0800

    js给按钮绑定多个事件

---
+++ 4/index.js
@@ -0,0 +1,19 @@
+var btn = document.getElementById("btn");
+function cs(){
+    console.log("cs");
+}
+var btn = document.getElementById("btn");
+btn.addEventListener("click",hello1);
+
+btn.addEventListener("click",hello2);
+
+ 
+
+function hello1(){
+    console.log("hello1");
+
+}
+
+function hello2(){
+    console.log("hello2");
+}
