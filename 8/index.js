58f4c0df3a344ef62cd25cde9c6c06dd724150de
icepluto <18792689010@163.com>
2019-04-29 17:51:37 +0800

    数组求和

---
+++ 8/index.js
@@ -0,0 +1,30 @@
+var arr1 = [1,2,2,3,1,4,5,6,4,7,71,8];
+var sum1 = 0;
+for (let i = 0; i < arr1.length; i++) {
+    if(typeof arr1[i] == "number"){
+    sum1 += arr1[i];
+    }
+    
+} 
+document.write(arr1 + "<br>");
+document.write(sum1 + "<br>");
+//方法二
+function sum(arr1){
+    var result = 0;
+    for(var i = 0; i<arr1.length; i++){
+        if(typeof arr1[i] == "number"){
+            result += arr1[i];
+        }
+    }
+    return result;
+}
+document.write(sum(arr1));
+
+
+
+
+
+
+
+
+
